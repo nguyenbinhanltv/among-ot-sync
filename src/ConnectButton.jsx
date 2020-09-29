87b82253f87be70e622b17de65c8aac6832574dc
Nguyễn Bình An <nguyenbinhanltv@gmail.com>
2020-09-30 03:03:50 +0700

    add connect button

---
+++ src/ConnectButton.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+import { syncIdLength } from "./config.json";
+import Button from "./Button";
+import styled from "styled-components";
+
+const ConnectButtonWrapper = styled(Button)`
+  width: 100%;
+`;
+
+export default function ConnectButton({ syncId, connectToGame, loading }) {
+  return (
+    <ConnectButtonWrapper
+      disabled={(syncId || "").length !== syncIdLength || loading}
+      onClick={connectToGame}
+    >
+      Connect
+    </ConnectButtonWrapper>
+  );
+}
