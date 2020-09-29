8055ac212ca5d3a2193ece3a0f5d1f4362b4da20
Nguyễn Bình An <nguyenbinhanltv@gmail.com>
2020-09-30 03:05:54 +0700

    add code page

---
+++ src/CodePage.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+import Title from "./Title";
+import CodeInput from "./CodeInput";
+import ConnectButton from "./ConnectButton";
+import VerticalContainer from "./VerticalContainer";
+import DiscordButton from "./DiscordButton";
+
+function CodePage({ syncId, setSyncId, connectToGame, loading }) {
+  return (
+    <VerticalContainer centerVertically>
+      <Title className="title" />
+      <div>
+        <CodeInput syncId={syncId} setSyncId={setSyncId} />
+        <ConnectButton
+          loading={loading}
+          syncId={syncId}
+          connectToGame={connectToGame}
+        />
+        <DiscordButton />
+      </div>
+    </VerticalContainer>
+  );
+}
+
+export default CodePage;
