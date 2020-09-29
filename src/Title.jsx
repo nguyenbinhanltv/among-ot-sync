86cf58ffbdd2276c5605211f0af7032da08d2955
Nguyễn Bình An <nguyenbinhanltv@gmail.com>
2020-09-30 03:05:02 +0700

    add title

---
+++ src/Title.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Title({ className }) {
+  return (
+    <div className={className}>
+      <img src="logo192.png" height="110" alt="Among-ot Icon" />
+      <br />
+    </div>
+  );
+}
