9cab264c98baebe0a00d1565e44e60e36a8e3cc2
Nguyễn Bình An <nguyenbinhanltv@gmail.com>
2020-09-30 03:04:07 +0700

    add discord button

---
+++ src/DiscordButton.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+import styled from "styled-components";
+import LinkButton from "./LinkButton";
+
+const DiscordIcon = styled.img`
+  height: 15px;
+  margin-right: 10px;
+`;
+
+export default function DiscordButton() {
+  return (
+    <LinkButton color="#7289DA" link="https:///" margin>
+      <DiscordIcon src="discord.svg" alt="" />
+      Add the bot to your server
+    </LinkButton>
+  );
+}
