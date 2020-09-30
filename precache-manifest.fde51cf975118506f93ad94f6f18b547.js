470b3bfe089b1bf8620bd62ba21c1da0e165961f
Nguyễn Bình An <nguyenbinhanltv@gmail.com>
2020-09-30 19:11:02 +0700

    Updates

---
+++ precache-manifest.fde51cf975118506f93ad94f6f18b547.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15b33e92fc8ba942ef7163e6fb01b52b",
+    "url": "/among-ot-sync/index.html"
+  },
+  {
+    "revision": "0c8ca81fa0493ccb4306",
+    "url": "/among-ot-sync/static/css/main.f1ae203c.chunk.css"
+  },
+  {
+    "revision": "45c03d1cee1cdf649084",
+    "url": "/among-ot-sync/static/js/2.06c7ba0e.chunk.js"
+  },
+  {
+    "revision": "7880e528e8c21721ffc6e103c03ca023",
+    "url": "/among-ot-sync/static/js/2.06c7ba0e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c8ca81fa0493ccb4306",
+    "url": "/among-ot-sync/static/js/main.ef27e1a6.chunk.js"
+  },
+  {
+    "revision": "edafd511dfd6fe9f1c30",
+    "url": "/among-ot-sync/static/js/runtime-main.993e83d5.js"
+  }
+]);
