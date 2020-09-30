3ad5ae69bde923b09492faba47bc81f34fdf1d55
Nguyễn Bình An <nguyenbinhanltv@gmail.com>
2020-09-30 19:03:47 +0700

    update

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const publicPath = path.join(__dirname, '..', 'build');
 const port = process.env.PORT || 3000;
 app.use(express.static(publicPath));
 app.get('*', (req, res) => {
-  res.sendFile(path.join(publicPath, 'index.html'));
+  res.sendFile(path.join(publicPath,'build', 'index.html'));
 });
 app.listen(port, () => {
   console.log('Server is up on port: ' + port);
